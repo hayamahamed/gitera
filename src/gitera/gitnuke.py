41cd614201e9f156ea91700b7f451f514dd0d843
hayam ahamed <hayam@duck.com>
2026-08-28 19:22:15 +0530

    gn fix
    main wasnt present in gn to work properly now fixed

--- src/gitera/gitnuke.py
+++ src/gitera/gitnuke.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 import subprocess
 
-try:
-    cm = int(input("How many commits you want? "))
-except ValueError:
-    print("Invalid input. Proceeding with default 100 commits")
-    cm = 100
 
-for i in range(cm):
-    subprocess.run(
-        [f'echo "{i}" >> gitout && git add . && git commit -m "Test commit {i}"'],
-        shell=True,
-        capture_output=True,
-        check=False,
-    )
-    subprocess.run(["rm gitout"], shell=True, check=False)
+def inp():
+    try:
+        cm = int(input("How many commits you want? "))
+    except ValueError:
+        print("Invalid input. Proceeding with default 100 commits")
+        cm = 100
+
+    for i in range(cm):
+        subprocess.run(
+            [f'echo "{i}" >> gitout && git add . && git commit -m "Test commit {i}"'],
+            shell=True,
+            capture_output=True,
+            check=False,
+        )
+        subprocess.run(["rm gitout"], shell=True, check=False)
+
+
+def main():
+    inp()
+
+
+if __name__ == "__main__":
+    main()
